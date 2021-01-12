b9ee5fa051faf57dd7a226eda7c9b355e71de45d
EricShi1255 <eshi20@stuy.edu>
2021-01-12 17:58:06 -0500

    nth method, tested a bit

---
+++ Radix.java
@@ -0,0 +1,20 @@
+public class Radix {
+    public static int nth(int n, int col) {
+        String num = Integer.toString(n);
+        int result = 0;
+        if (num.length()-col <= num.length() && num.length()-col-1 >= 0) {
+            result =  Character.getNumericValue(num.charAt(num.length() - 1 - col));
+        }
+        return result;
+    }  
+    
+    public static int length(int n) {
+        return 4;
+    }
+    public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
+
+    }
+    public static void main(String[] args) {
+        System.out.println(nth(65565253,10));
+    }
+}
