3710927671c66a2512ba4eda7d4c74e7c08bafa5
EricShi1255 <eshi20@stuy.edu>
2021-01-12 23:26:47 -0500

    need to find # of passses to make

--- Radix.java
+++ Radix.java
@@ -25,6 +25,7 @@ public class Radix {
     public static void radixSortSimple(SortableLinkedList list) {
         //Assume there are no negative values.
         int digits = length(list.get(0));
+        int largest = list.get(0);
         for (int i = 0; i < digits; i++) { //how many passes to make
             
                 SortableLinkedList a0 = new SortableLinkedList();
@@ -41,6 +42,11 @@ public class Radix {
                 SortableLinkedList[] bucket =  {a0, b1, c2, d3, e4, f5, g6, h7, i8, j9};
 
             for (int j = 0; j < list.size(); j++) { //looping over the array
+                //largest number
+                if (i = 0) {
+
+                }
+                //actual sorting
                 int eachnumber = list.get(j);
                 int eachdigit = nth(eachnumber, i);
                 
@@ -51,8 +57,53 @@ public class Radix {
             merge(list, bucket);
         }
     } 
-    public static void radixSort(SortableLinkedList data) {
+    public static void radixSort(SortableLinkedList list) {
+        int digits = length(list.get(0));
+        for (int i = 0; i < digits; i++) { //how many passes to make
+            
+                //normal
+                SortableLinkedList a0 = new SortableLinkedList();
+                SortableLinkedList b1 = new SortableLinkedList();
+                SortableLinkedList c2 = new SortableLinkedList();
+                SortableLinkedList d3 = new SortableLinkedList();
+                SortableLinkedList e4 = new SortableLinkedList();
+                SortableLinkedList f5 = new SortableLinkedList();
+                SortableLinkedList g6 = new SortableLinkedList();
+                SortableLinkedList h7 = new SortableLinkedList();
+                SortableLinkedList i8 = new SortableLinkedList();
+                SortableLinkedList j9 = new SortableLinkedList();
+                //negative only
+                SortableLinkedList aa = new SortableLinkedList();
+                SortableLinkedList bb = new SortableLinkedList();
+                SortableLinkedList cc = new SortableLinkedList();
+                SortableLinkedList dd = new SortableLinkedList();
+                SortableLinkedList ee = new SortableLinkedList();
+                SortableLinkedList ff = new SortableLinkedList();
+                SortableLinkedList gg = new SortableLinkedList();
+                SortableLinkedList hh = new SortableLinkedList();
+                SortableLinkedList ii = new SortableLinkedList();
+                SortableLinkedList jj = new SortableLinkedList();
 
+                SortableLinkedList[] bucket =  {a0, b1, c2, d3, e4, f5, g6, h7, i8, j9};
+                SortableLinkedList[] negativebucket = {aa, bb, cc, dd, ee, ff, gg, hh, ii, jj};
+
+            for (int j = 0; j < list.size(); j++) { //looping over the array
+                int eachnumber = list.get(j);
+                int eachdigit = nth(eachnumber, i);
+
+                char negative = (Integer.toString(eachnumber)).charAt(0);
+                if (negative != '-') {
+                    bucket[eachdigit].add(eachnumber);
+                }
+                else {
+                    negativebucket[10-eachdigit].add(-1*eachnumber);
+                }
+                list.remove(j);
+                j--;
+            }
+            merge(list, negativebucket);
+            merge(list, bucket);
+        } 
     }
 
     
